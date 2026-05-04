2ea3ca17d1ee4e7b42a2a3e349e0dbd14fa9e353
anushka251306 <anushka.251306@ncit.edu.np>
2026-05-05 00:33:35 +0545

    refactor menu to switch -case and implement file-based account creation

--- main.c
+++ main.c
@@ -15,6 +15,39 @@ void showMenu(){
     printf("5.Exit");
     printf("Enter choice:\n");
 }
+void createAccount(){
+    FILE *fp;
+    fp=fopen("account.dat","ab");
+    struct account a;
+    if(fp==NULL){
+        printf("file cannot be opened\n");
+        return 1;
+    }
+    printf("Enter the account number\n");
+    scanf("%d", & a.accno);
+    printf("Enter the name\n");
+    scanf("%s",a.name);
+    balance=0.0;//new accout opens with zero balance
+    fwrite(& account,sizeof(struct account),1,fp);
+    printf("Account Created Successfully!!\n");
+}
 int main(){
-    printf("\nBank System Initialized\n");
+    while (1){
+  showMenu();
+  scanf("%d",& choice);
+  switch(choice){
+if(choice==1){
+    createAccount();
+}
+else if(choice==2){
+    break;
+}
+else{
+    printf("\n invalid choice!\n");
+}
+default:
+                printf("Invalid choice! Please enter 1 or 2.\n");
+    }
+}
+return 0;
 }
